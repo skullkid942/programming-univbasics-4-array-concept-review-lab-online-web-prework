5a05d018ba63b16c7adca391eef25daec34b35d6
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-29 19:35:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,14 +11,14 @@ def find_element_index(array,value_to_find)
   return index 
 end
 
-def find_max_value(scale)
-  scale.sort
-  max = scale.last 
+def find_max_value(scale_2)
+  scale_2.sort
+  max = scale_2.last 
   p max
 end
 
-def find_min_value(scale)
-  scale.sort
-  min = scale.first 
+def find_min_value(scale_2)
+  scale_2.sort
+  min = scale_2.first 
   p min 
 end
