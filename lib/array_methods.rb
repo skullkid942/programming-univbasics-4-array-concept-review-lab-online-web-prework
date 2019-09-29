8c04a11c239043040b575e79b1436c8bce2367b7
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-29 19:33:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,14 +11,14 @@ def find_element_index(array,value_to_find)
   return index 
 end
 
-def find_max_value(array)
-  array.sort
-  max = array.last 
+def find_max_value(scale)
+  scale.sort
+  max = scale.last 
   p max
 end
 
-def find_min_value(array)
-  array.sort
-  min = array.first 
+def find_min_value(scale)
+  scale.sort
+  min = scale.first 
   p min 
 end
